688874bf4bb2a8b0cc7059e0fc1fbb60c3d2794d
rmesterca <raluca@egeniq.com>
2022-10-13 00:03:25 +0200

    singleton simplified in swift

--- EssentialDeveloper/EssentialDeveloper/Practice/M1-L1-Singleton/Singleton.swift
+++ EssentialDeveloper/EssentialDeveloper/Practice/M1-L1-Singleton/Singleton.swift
@@ -7,17 +7,32 @@
 
 import UIKit
 
-class ApiClient {
+// singleton by the book (GOF)
+let client = ApiClient1.getInstance()
+
+class ApiClient1 {
 
     // the key point is that this class needs to have a single point of access
-    private static let instance = ApiClient()
+    private static let instance = ApiClient1()
 
     private init() { }
 
-    static func getInstance() -> ApiClient {
+    static func getInstance() -> ApiClient1 {
         return instance
     }
-
 }
 
-let client = ApiClient.getInstance()
+// in Swift we can just use static let because it cannot be mutate
+// the type system already enforces that for us - simple implementation
+let client = ApiClient2.instance
+
+class ApiClient2 {
+
+    // the key point is that this class needs to have a single point of access
+    private static let instance = ApiClient2()
+
+    private init() { }
+}
+
+
+
