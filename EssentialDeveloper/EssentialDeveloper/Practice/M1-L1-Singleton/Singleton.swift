f522866d45a3918429822b9e9400c19e8c0699f2
rmesterca <raluca@egeniq.com>
2022-10-13 00:10:34 +0200

    singleton - swift - subclassing or not

--- EssentialDeveloper/EssentialDeveloper/Practice/M1-L1-Singleton/Singleton.swift
+++ EssentialDeveloper/EssentialDeveloper/Practice/M1-L1-Singleton/Singleton.swift
@@ -35,4 +35,38 @@ class ApiClient2 {
 }
 
 
+// the books says you should not make a singleton a final class
+// because it may be extended in the future
+// pattern specifies we should allow extension
+// i.e. adding more methods to the APIClient by having a subclass
+let client3 = ApiClient3.instance
+
+final class ApiClient3 {
+
+    // the key point is that this class needs to have a single point of access
+    static let instance = ApiClient3()
+
+    private init() { }
+}
+
+//class MyApiClient: ApiClient3 {
+//
+//    // add more methods or override some behaviour
+//}
+
+// However in Swift we can use extensions - no need for subclassing
+
+extension ApiClient3 {
+
+    // trade off:
+    // we cannot override behaviour, but we can still add more behaviour
+}
+
+// if you want to allow others to change the behavior you allow subclassing
+// otherwise just use extensions
+
+
+
+
+
 
